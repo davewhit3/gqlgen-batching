896e21db52cbcf96c9fc9f7860641bb574c4e46e
pixlcrashr <vin@centheins.de>
2024-05-13 00:13:10 +0200

    fix: json Number struct parsing

--- handler.go
+++ handler.go
@@ -19,17 +19,24 @@ func (c GraphqlRawParamsCollection) Len() int {
 
 // UnmarshalJSON could unmarshal slice or single query
 func (c *GraphqlRawParamsCollection) UnmarshalJSON(b []byte) error {
-	err := json.Unmarshal(b, (*[]*graphql.RawParams)(c))
-	if err == nil {
+	r := bytes.NewReader(b)
+
+	d := json.NewDecoder(r)
+	d.UseNumber()
+
+	if err := d.Decode((*[]*graphql.RawParams)(c)); err == nil {
 		return nil
+	}
+
+	if _, err := r.Seek(0, 0); err != nil {
+		return err
 	}
 
 	var n graphql.RawParams
-	err = json.Unmarshal(b, &n)
-	if err == nil {
-		*c = []*graphql.RawParams{&n}
+	if err := d.Decode(&n); err != nil {
 		return nil
 	}
 
+	*c = []*graphql.RawParams{&n}
 	return nil
 }
